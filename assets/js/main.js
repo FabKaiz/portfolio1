06116353669c52c2aba6e3adfabe9570a4000343
Fabien <cacrfabfr@gmail.com>
2021-09-09 12:24:21 +0200

    Added Dark theme mode and scroll style

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-/*==================== JS Plugins ====================*/
+/*==================== JS PLUGINS / SECTIONS ====================*/
 import "./plugins/nav.js";
 import "./plugins/accordion_skills.js";
 import "./plugins/qualification.js";
@@ -9,9 +9,11 @@ import "./plugins/contact.js";
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 import "./plugins/scroll_section_links.js"
 
-
-/*==================== CHANGE BACKGROUND HEADER ====================*/
+/*==================== ADD SHADOW TO NAV WHEN SCROLLING ====================*/
+import "./plugins/add_shadow_scroll.js"
 
 /*==================== SHOW SCROLL UP ====================*/
+import "./plugins/show_scroll_btn.js"
 
 /*==================== DARK LIGHT THEME ====================*/
+import "./plugins/dark_light_theme.js";
