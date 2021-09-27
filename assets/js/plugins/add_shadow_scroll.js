4030ff9dcada6c178737238fe8c6589dd2120141
Fabien <cacrfabfr@gmail.com>
2021-09-27 13:50:28 +0200

    fix header scroll animation and active class

--- assets/js/plugins/add_shadow_scroll.js
+++ assets/js/plugins/add_shadow_scroll.js
@@ -1,7 +1,7 @@
 /*==================== ADD SHADOW TO NAV WHEN SCROLLING ====================*/
 function scrollHeader(){
   const nav = document.getElementById('header')
-  // When the scroll is greater than 10 viewport height, add the scroll-header class to the header tag
-  if(this.scrollY >= 10) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+  // When the scroll is greater than 40 viewport height, add the scroll-header class to the header tag
+  if(this.scrollY >= 40) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
